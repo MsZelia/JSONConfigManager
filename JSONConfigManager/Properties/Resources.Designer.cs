2382c789117ac81ca684a2f6eac3ce7a298ea2e7
MsZelia <nozmvnaml@protonmail.com>
2025-08-01 17:17:19 +0200

    Added restore backup icon

--- JSONConfigManager/Properties/Resources.Designer.cs
+++ JSONConfigManager/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace JSONConfigManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Restore {
+            get {
+                object obj = ResourceManager.GetObject("Restore", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap SaveFile {
             get {
                 object obj = ResourceManager.GetObject("SaveFile", resourceCulture);
